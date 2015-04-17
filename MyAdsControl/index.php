094ed0ceb818110d086fac23d55870ccb42a3591
hoangwin <hoang.nguyenmau@hotmail.com>
2015-04-17 09:04:27 +0700

    ok

--- MyAdsControl/index.php
+++ MyAdsControl/index.php
@@ -11,6 +11,14 @@
 	{
 		include_once "bantrungkhunglong_thuanvietgame.php";
 	}
-	
+	else if($game =="ONET_3D")//link
+	{
+		include_once "onet_3d.php";
+	}
+	else if($game =="BAN_CA_AN_XIENG")//link
+	{
+		include_once "ban_ca_an_xieng.php";
+//		include_once "onet_3d.php";
+	}	
 
 ?>
